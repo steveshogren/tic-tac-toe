73f98bb1f8b7531359ad1f64a003e20a83edfd99
jack <steve.a.shogren@gmail.com>
2016-09-28 07:19:04 -0400

    Clean up the game a bit

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main my-game.core
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/tools.cli "0.3.5"]])
--- src/my_game/core.clj
+++ src/my_game/core.clj
@@ -1,4 +1,5 @@
 (ns my-game.core
+  (:require [clojure.tools.cli :refer [parse-opts]])
   (:gen-class))
 
 (def empty-map [[" " " " " "] [" " " " " "] [" " " " " "]])
@@ -7,18 +8,15 @@
   (seq (map println m))
   (println "test"))
 
+(defn next-player [p] (if (= p "X") "O" "X"))
+(defn player-wins? [m] false)
+
 (defn mark-cell [m c r contents]
   (let [c (Integer/parseInt c)
         r (Integer/parseInt r)
-        old-row (get m c)
+        old-row (get m r)
         new-row (assoc old-row c contents)]
     (assoc m r new-row)))
-
-(defn next-player [p]
-  (if (= p "X") "O" "X"))
-
-(defn player-wins? [m]
-  false)
 
 (defn start-game []
   (println "Start Game!")
@@ -28,10 +26,19 @@
     (println "Player " player " move:")
     (let [input (read-line)
           [c r] (.split input " ")
+          ;; MUTATION BY ANY OTHER NAME...
           m (mark-cell m c r player)]
       (if (player-wins? m)
         (println "Player " player " Wins!")
         (recur m (next-player player))))))
 
+(def cli-options
+  [["-p" "--play" "Play game"
+    :id :play]
+   ["-h" "--help" "Help"
+    :id :help]])
+
 (defn -main [& args]
-  (start-game))
+  (let [opts (:options (parse-opts args cli-options))]
+    (cond (:play opts) (start-game)
+          (:help opts) (println "Use -p to play game"))))
